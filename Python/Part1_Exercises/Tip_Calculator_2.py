a326c21d97874642f905b73e992870bb917bef9b
Gregory Neal <gnealsr@gmail.com>
2017-10-05 08:52:38 -0500

    10-05-17 AM commit

--- Python/Part1_Exercises/Tip_Calculator_2.py
+++ Python/Part1_Exercises/Tip_Calculator_2.py
@@ -1,3 +1,18 @@
+
+import tornado.log
+import tornado.ioloop
+import tornado.web
+from jinja2 import Environment, PackageLoader
+
+ENV = Environment(
+    loader=PackageLoader('tipapp', 'templates')
+)
+
+class MainHandler(tornado.web.RequestHandler):
+  def get(self):
+    self.set_header("Content-Type", 'text/plain')
+    self.write("Hello, world")
+
 total_bill = float(input('Please enter total bill amount? '))
 level_of_service = input('Please enter level of service - (Good) (Fair) (Bad)? ')
 split_number_of_ways = int(input('Split how many ways? '))
@@ -17,3 +32,18 @@ split_amount = total_bill / split_number_of_ways
 print('Tip amount: ' + "${:.2f}".format(tip))
 print('Total amount: ' + "${:.2f}".format(total_bill))
 print('Amount per person: ' + "${:.2f}".format(split_amount))
+
+def make_app():
+  return tornado.web.Application([
+    (r"/", MainHandler)
+    ], autoreload=True)
+
+# Main
+
+if __name__ == "__main__":
+
+    tornado.log.enable_pretty_logging()
+
+    app = make_app()
+    app.listen(8888, print('Hosting at 8888'))
+    tornado.ioloop.IOLoop.current().start()
